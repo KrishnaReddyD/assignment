1a830143b5c85acbe90d4d99f915a9db0d09e308
KrishnaReddyD <krishnadev0456@gmail.com>
2023-04-10 15:28:05 -0500

    added files

---
+++ elc-coding-test-master-d34f6ab20275bec4da01e77610a15dffb8a76c4c/server/app.js
@@ -0,0 +1,47 @@
+/**
+ * The Server Can be configured and created here...
+ * 
+ * You can find the JSON Data file here in the Data module. Feel free to impliment a framework if needed.
+ */
+
+/*
+-- This is the product data, you can view it in the file itself for more details 
+{
+    "_id": "019",
+    "isActive": "false",
+    "price": "23.00",
+    "picture": "/img/products/N16501_430.png",
+    "name": "Damage Reverse Thickening Conditioner",
+    "about": "Dolor voluptate velit consequat duis. Aute ad officia fugiat esse anim exercitation voluptate excepteur pariatur sit culpa duis qui esse. Labore amet ad eu veniam nostrud minim labore aliquip est sint voluptate nostrud reprehenderit. Ipsum nostrud culpa consequat reprehenderit.",
+    "tags": [
+        "ojon",
+        "conditioner"
+    ]
+}
+*/
+const data = require('./data');
+const http = require('http');
+const hostname = 'localhost';
+const port = 3035;
+
+/** 
+ * Start the Node Server Here...
+ * 
+ * The http.createServer() method creates a new server that listens at the specified port.  
+ * The requestListener function (function (req, res)) is executed each time the server gets a request. 
+ * The Request object 'req' represents the request to the server.
+ * The ServerResponse object 'res' represents the writable stream back to the client.
+ */
+http.createServer(function (req, res) {
+    // .. Here you can create your data response in a JSON format
+    const query = req.query;
+    const searchQuery = query.searchQuery;
+
+    const filteredData = data.filter(d => d.name.includes(searchQuery));
+
+    res.write(filteredData); // Write out the default response
+    res.end(); //end the response
+}).listen(port);
+
+
+console.log(`[Server running on ${hostname}:${port}]`);
